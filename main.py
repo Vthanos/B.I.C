a71b0f28ee20c7985d9a25df3d425c6ce19bec73
Chimi <thvaskadiras@gmail.com>
2022-07-16 12:46:54 +0300

    V1 of my model

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ import pandas as pd
 import os
 
 print("hey ")
+#v1 design of my model
+model = tf.keras.Sequential([
+tf.keras.layers.Conv2D(32, (3, 3), padding = 'same', activation = 'relu', input_shape = (50, 50, 3)),
+tf.keras.layers.MaxPooling2D(strides = 2),
+tf.keras.layers.Conv2D(64, (3, 3), padding = 'same', activation = 'relu'),
+tf.keras.layers.MaxPooling2D((3, 3),strides = 2),
+tf.keras.layers.Conv2D(128, (3, 3), padding = 'same', activation = 'relu'),
+tf.keras.layers.MaxPooling2D((3, 3),strides =2),
+tf.keras.layers.Conv2D(128, (3, 3), padding = 'same', activation = 'relu'),
+tf.keras.layers.MaxPooling2D((3, 3),strides =2),
+tf.keras.layers.Flatten(),
+tf.keras.layers.Dense(128, activation = 'relu'),
+tf.keras.layers.Dense(2, activation = 'softmax')
+])
+
+model.summary()
+
+#model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=['accuracy'])
+
+#history = model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 25 , batch_size = 75)
+
+
